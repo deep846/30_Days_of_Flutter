f7f3d1c44b156739c174824cb4fc7935135e7808
deep846 <45318452+deep846@users.noreply.github.com>
2021-05-29 21:48:23 +0530

    day 6 complete

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:my_first_app/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+String name = "";
+bool pressed = false;
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -19,7 +27,7 @@ class LoginPage extends StatelessWidget {
               height: 20,
             ),
             Text(
-              "Welcome",
+              "Welcome $name",
               style: TextStyle(
                 //fontStyle: ,
                 fontSize: 40,
@@ -34,10 +42,16 @@ class LoginPage extends StatelessWidget {
                   const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
               child: Column(
                 children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter UserName",
-                      labelText: "UserName",
+                  Center(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter UserName",
+                        labelText: "UserName",
+                      ),
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
                     ),
                   ),
                   TextFormField(
@@ -50,17 +64,49 @@ class LoginPage extends StatelessWidget {
                   SizedBox(
                     height: 40.0,
                   ),
-                  ElevatedButton(
-                    style: TextButton.styleFrom(
-                        minimumSize: Size(150,
-                            50)), //construct a button with adjusting hight and width
-                    onPressed: () {
-                      print("Hi Deep");
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        pressed = true;
+                      });
+                      await Future.delayed(Duration(seconds: 1));
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
-
-                    child: Text("Login"),
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      height: 50,
+                      width: pressed ? 50 : 150,
+                      alignment: Alignment.center,
+                      child: pressed
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "Login",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                      decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          // shape: pressed ? BoxShape.circle : BoxShape.rectangle,
+                          borderRadius:
+                              BorderRadius.circular(pressed ? 50 : 10)),
+                    ),
                   )
+                  // ElevatedButton(
+                  //   style: TextButton.styleFrom(
+                  //       minimumSize: Size(150,
+                  //           50)), //construct a button with adjusting hight and width
+                  //   onPressed: () {
+                  //     print("Hi Deep");
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  //   child: Text("Login"),
+                  // )
                 ],
               ),
             ),
